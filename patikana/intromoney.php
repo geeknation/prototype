cd2dcff9089b5f4f8d10fe08921e680007b5d3b3
geeknation <ianmukunya@gmail.com>
2014-02-01 11:01:26 +0300

    second commit

---
+++ patikana/intromoney.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Patikana||collector</title>
+		<script type="text/javascript" src="jquery_ptk/jquery.js"></script>
+		<script type="text/javascript" src="jquery_ptk/jqueryui.js"></script>
+		<link rel="stylesheet" type="text/css" href="bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="jquery_ptk/jquery.css">
+		<link rel="stylesheet" type="text/css" href="flat-ui.css">
+		<link rel="stylesheet" type="text/css" href="template.css">
+		<script type="text/javascript" src="livepage.js"></script>
+	</head>
+	<body>
+		<?php
+		include "header.php";
+		?>
+		<div id="wrap" style="padding:1%;">
+			<div id="carouselholder">
+				<div class="hero-unit">
+					<h2>Welcome to Patikana finder network</h2>
+					<p>
+						Help thousand of people find their lost documents, and the best part of it all...You get paid
+					</p>
+					<input type="button" class="btn btn-success btn-large" value="Get Started"/>
+				</div>
+			</div>
+			<hr />
+		</div>
+	</body>
+</html>
